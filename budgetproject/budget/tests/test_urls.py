857b2319de3ab2d2b995d1c8822d95cf0a2fc934
Daniel Diaz <danieldiazgonza52@gmail.com>
2021-03-13 16:08:28 -0500

    added Project and Url tests

---
+++ budgetproject/budget/tests/test_urls.py
@@ -0,0 +1,36 @@
+from django.test import TestCase, SimpleTestCase
+
+from django.urls import reverse, resolve
+
+from budget.views import *
+
+# Create your tests here.
+
+# Url patterns
+# urlpatterns = [
+#     path('', views.project_list, name='list'),
+#     path('add/', views.ProjectCreateView.as_view(), name='add'),
+#     path('<slug:project_slug>/', views.project_detail, name='detail')
+# ]
+
+
+class TestUrls(SimpleTestCase):
+    
+    def test_list_url_resolves(self):
+        url = reverse("list")
+
+        print(resolve(url))
+        # Asserts that the home url points to the view function fo project list
+        self.assertEquals(resolve(url).func, project_list)
+
+    def test_create_project_url_resolves(self):
+        url = reverse("add")
+        print(resolve(url))
+    
+        self.assertEquals(resolve(url).func.view_class, ProjectCreateView)
+    
+    def test_project_detail_url_resolves(self):
+        url = reverse("detail", args=["some-slug"])
+        print(resolve(url))
+
+        self.assertEquals(resolve(url).func, project_detail)
